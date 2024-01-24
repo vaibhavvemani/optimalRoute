6b49fed9b3a85745ea7a3ce37426c8131a7fa110
vaibhavvemani <vaibhavvemani@gmail.com>
2024-01-24 17:48:18 +0530

    Initial Commit

---
+++ api/index.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import requests
+import os
+
+app = Flask(__name__)
+maps_api_key = os.environ.get('MAPS_API_KEY')
+
+@app.get('/getroute')
+def sendRequest():
+    data = requests.post(
+        "https://routes.googleapis.com/directions/v2:computeRoutes", 
+        headers={
+            'Content-Type': 'application/json',
+            'X-Goog-Api-Key': maps_api_key,
+            'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline',
+        },
+        data={
+            "origin":{
+                "placeId": "ChIJayOTViHY5okRRoq2kGnGg8o"
+            },
+            "destination":{
+                "placeId": "ChIJTYKK2G3X5okRgP7BZvPQ2FU"
+            },
+            "travelMode": "DRIVE",
+            "routingPreference": "TRAFFIC_AWARE",
+            "departureTime": "2024-10-15T15:01:23.045123456Z",
+            "computeAlternativeRoutes": False,
+            "routeModifiers": {
+                "avoidTolls": False,
+                "avoidHighways": False,
+                "avoidFerries": False
+            },
+            "languageCode": "en-US",
+            "units": "IMPERIAL"
+        }
+    )
+    return data
+
+
